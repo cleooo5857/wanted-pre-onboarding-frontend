460b565a61cd74aa92ac33375a595ab74cfd87ce
cleooo5857 <zleooo@naver.com>
2023-08-14 14:20:21 +0900

    [Comment] useAuthRedirect 파일 주석처리

--- src/hook/useAuthRedirect.js
+++ src/hook/useAuthRedirect.js
@@ -4,7 +4,7 @@ import { TokenRepository } from "../repository/TokenRepository";
 
 function useRedirect(redirectTo) {
   const navigate = useNavigate();
-
+  // 로컬스토리지에 token 있는지 확인
   useEffect(() => {
     if (TokenRepository.getToken()) {
       navigate(redirectTo, { replace: true });
